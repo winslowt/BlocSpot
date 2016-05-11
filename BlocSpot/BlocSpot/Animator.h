fa810be495438f273fa64218aa5f543efd50dfcc
Tony Winslow <winslowt1@nku.edu>
2016-05-10 20:35:21 -0400

    5 10

---
+++ BlocSpot/BlocSpot/Animator.h
@@ -0,0 +1,13 @@
+//
+//  Animator.h
+//  BlocSpot
+//
+//  Created by Tony  Winslow on 5/8/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animator : NSObject
+
+@end
---
+++ BlocSpot/BlocSpot/Animator.m
@@ -0,0 +1,13 @@
+//
+//  Animator.m
+//  BlocSpot
+//
+//  Created by Tony  Winslow on 5/8/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "Animator.h"
+
+@implementation Animator
+
+@end
